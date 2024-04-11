a2f14ebdfc6585bcd6f155aec182f3fc05e51775
kumargauravsagar <156787586+kumargauravsagar@users.noreply.github.com>
2024-04-11 11:46:50 +0530

    Update codeFile.py
    2nd version of my code

--- codeFile.py
+++ codeFile.py
@@ -1,119 +1,137 @@
 import random
+import string
 
-def choose_word():
-    words = ['python', 'hangman', 'game', 'programming', 'computer']
-    return random.choice(words)
+def choose_word(difficulty):
+    if difficulty == "easy":
+        words = ["apple", "banana", "orange", "grape", "pear"]
+    elif difficulty == "medium":
+        words = ["python", "java", "javascript", "ruby", "php"]
+    else:
+        words = ["hangman game", "machine learning", "openai", "artificial intelligence", "deep learning"]
+    return random.choice(words).lower()
 
 def display_word(word, guessed_letters):
-    displayed_word = ''
+    displayed_word = ""
     for letter in word:
-        if letter in guessed_letters:
+        if letter in guessed_letters or letter == ' ':
             displayed_word += letter
         else:
-            displayed_word += '_'
+            displayed_word += "_"
     return displayed_word
 
-def display_hangman(attempts):
-    stages = [
-        '''
-        -------
-        |     |
-        |     O
-        |    \|/
-        |     |
-        |    / \\
-        -
-        ''',
-        '''
-        -------
-        |     |
-        |     O
-        |    \|/
-        |     |
-        |    / 
-        -
-        ''',
-        '''
-        -------
-        |     |
-        |     O
-        |    \|/
-        |     |
-        |     
-        -
-        ''',
-        '''
-        -------
-        |     |
-        |     O
-        |    \|
-        |     |
-        |     
-        -
-        ''',
-        '''
-        -------
-        |     |
-        |     O
-        |     |
-        |     |
-        |     
-        -
-        ''',
-        '''
-        -------
-        |     |
-        |     O
-        |    
-        |     
-        |     
-        -
-        ''',
-        '''
-        -------
-        |     |
-        |     
-        |    
-        |     
-        |     
-        -
-        '''
+def display_available_letters(guessed_letters):
+    available_letters = [letter for letter in string.ascii_lowercase if letter not in guessed_letters]
+    return ' '.join(available_letters)
+
+def display_hangman(turns):
+    stages = [  
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |      
+                   -
+                """,
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|
+                   |      |
+                   |     
+                   -
+                """,
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
     ]
-    print(stages[attempts])
+    return stages[turns]
 
 def hangman():
-    word = choose_word()
-    guessed_letters = []
-    attempts = 6
-
     print("Welcome to Hangman!")
-    print("The word has", len(word), "letters.")
-
+    
     while True:
-        print("\nWord:", display_word(word, guessed_letters))
-        print("Guessed letters:", guessed_letters)
-        print("Attempts left:", attempts)
-        display_hangman(attempts)
-
-        guess = input("Guess a letter: ").lower()
-
-        if not guess.isalpha() or len(guess) != 1:
-            print("Invalid input. Please enter a single alphabetic character.")
+        difficulty = input("Choose difficulty (easy/medium/hard): ").lower()
+        if difficulty not in ["easy", "medium", "hard"]:
+            print("Invalid difficulty level. Please choose easy, medium, or hard.")
             continue
+        word = choose_word(difficulty)
+        guessed_letters = []
+        attempts = 6
+        turns = 0
 
-        if guess in guessed_letters:
-            print("You've already guessed that letter.")
-            continue
-        elif guess in word:
-            guessed_letters.append(guess)
-            if set(word) == set(guessed_letters):
-                print("Congratulations! You guessed the word:", word)
-                break
+        print("\nTry to guess the word.")
+        while attempts > 0:
+            print("\nAttempts left:", attempts)
+            print(display_hangman(turns))
+            print(display_word(word, guessed_letters))
+            print("Available letters:", display_available_letters(guessed_letters))
+
+            guess = input("Enter a letter: ").lower()
+
+            if guess in guessed_letters:
+                print("You already guessed that letter.")
+            elif guess in word:
+                guessed_letters.append(guess)
+                if set(word.replace(' ', '')) == set(guessed_letters):
+                    print("Congratulations! You guessed the word:", word)
+                    break
+            else:
+                print("Incorrect guess.")
+                attempts -= 1
+                turns += 1
+
         else:
-            attempts -= 1
-            print("Incorrect guess.")
-            if attempts == 0:
-                print("Sorry, you're out of attempts. The word was:", word)
-                break
+            print(display_hangman(turns))
+            print("You ran out of attempts. The word was:", word)
+
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != "yes":
+            print("Thanks for playing Hangman!")
+            break
 
 hangman()
